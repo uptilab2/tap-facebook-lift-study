a671ea65b15441cd4820d9ad42efbab43798fdaf
zozorleponey <emile.caron@outlook.com>
2021-02-08 15:40:30 +0100

    wip

--- tap_facebook_lift_study.py
+++ tap_facebook_lift_study.py
@@ -22,11 +22,15 @@ REQUIRED_CONFIG_KEYS = [
 LOGGER = singer.get_logger()
 
 
+def get_abs_path(path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
+
 def load_schemas():
     """ Load schemas from schemas folder """
     schemas = {}
-    for filename in os.listdir('schemas'):
-        path = 'schemas/' + filename
+    for filename in os.listdir(get_abs_path('schemas')):
+        path = get_abs_path('schemas') + '/' + filename
         file_raw = filename.replace('.json', '')
         with open(path) as file:
             schemas[file_raw] = Schema.from_dict(json.load(file))
