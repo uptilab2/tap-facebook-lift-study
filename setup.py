ca5da3720e85952b195a34c8f95d494a082177e5
zozorleponey <emile.caron@outlook.com>
2021-02-08 15:43:57 +0100

    fix le project name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name="",
+    name="tap-facebook-lift-study",
     version="0.1.0",
     description="Singer.io tap for extracting data",
     author="emile.caron@uptilab.com",
